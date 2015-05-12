02013fa1dbfc0a6c4f1dee1e6cef7bfd7ebf0fcd
Falola Seyi <falolasheyie@gmail.com>
2015-05-12 11:45:23 +0100

    Learning Ruby

---
+++ word-count.rb
@@ -0,0 +1,14 @@
+puts "This is the text"
+strg= gets.chomp
+def wordcount(str)
+  words = str.split(" ")
+  frequency=Hash.new(0)
+  words.each do |word|
+  	frequency[word] +=1
+     end
+  frequency = frequency.sort_by do |x, y|
+  	y
+	end
+end
+
+puts wordcount(strg)
